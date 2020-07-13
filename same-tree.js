97764e6b3bc8b417d39c918f98b4f1dabb86916c
ardnahcivar <ravbhanage@gmail.com>
2020-07-13 23:44:22 +0530

    solved identify whether both binary tree's are same

---
+++ same-tree.js
@@ -0,0 +1,61 @@
+// Same Tree
+// Given two binary trees, write a function to check if they are the same or not.
+
+// Two binary trees are considered the same if they are structurally identical and the nodes have the same value.
+
+// Example 1:
+
+// Input:     1         1
+//           / \       / \
+//          2   3     2   3
+
+//         [1,2,3],   [1,2,3]
+
+// Output: true
+// Example 2:
+
+// Input:     1         1
+//           /           \
+//          2             2
+
+//         [1,2],     [1,null,2]
+
+// Output: false
+// Example 3:
+
+// Input:     1         1
+//           / \       / \
+//          2   1     1   2
+
+//         [1,2,1],   [1,1,2]
+
+// Output: false
+
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+var isSameTree = function(p, q) {
+    let same = true
+    dfs(p,q)
+    return same
+    
+    function dfs(p,q){
+        if((p != null && q == null) || (p == null && q != null) || (p && q && p.val != q.val)) {
+          same = false  
+        }else if(p && q){
+            dfs(p.left,q.left)
+            dfs(p.right,q.right)
+        }
+    }
+};
