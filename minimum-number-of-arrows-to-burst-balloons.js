d3be071c34be7fe30dbe56cf934733fd76a8db25
ravbhanage <ravbhanage@gmail.com>
2022-01-13 20:49:38 +0530

    added another approach to minimum number of arrows to burst ballons problem

--- minimum-number-of-arrows-to-burst-balloons.js
+++ minimum-number-of-arrows-to-burst-balloons.js
@@ -59,3 +59,26 @@ var findMinArrowShots = function(points) {
     }
     return arrows  
 };
+
+var findMinArrowShotsAnotherApproach = function(points) {
+    if(points.length === 0){
+        return 0;
+    }
+    
+    if(points.length === 1){
+        return 1;
+    }
+    
+    points.sort((a,b) => a[1] - b[1]);
+    let prev = points[0][1], arrows = 1;
+    
+    for(let i = 0; i < points.length; i++){
+        if(prev < points[i][0]){
+            arrows++;
+            prev = points[i][1]
+        }
+    }
+    
+    return arrows;
+    
+};
