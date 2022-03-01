3fe76e165590d390d791de51c83540e6ae79c9fb
ravbhanage <ravbhanage@gmail.com>
2022-03-01 20:30:47 +0530

    counting bits and single number problem

---
+++ counting-bits.js
@@ -0,0 +1,54 @@
+/* Counting Bits
+
+Given an integer n, return an array ans of length n + 1 such that for each i (0 <= i <= n),
+ans[i] is the number of 1's in the binary representation of i.
+
+Example 1:
+Input: n = 2
+Output: [0,1,1]
+Explanation:
+0 --> 0
+1 --> 1
+2 --> 10
+
+Example 2:
+Input: n = 5
+Output: [0,1,1,2,1,2]
+Explanation:
+0 --> 0
+1 --> 1
+2 --> 10
+3 --> 11
+4 --> 100
+5 --> 101
+
+Constraints:
+    0 <= n <= 105
+*/
+
+/**
+ * @param {number} n
+ * @return {number[]}
+ */
+ var countBits = function(n) {
+    let counts = [0];
+    
+    if(n === 0){
+        return counts;
+    }
+    
+    for(let i = 1; i <= n; i++){
+        let curr = i;
+        let tempCount = 0;
+        
+        while(curr > 0){
+            let one = curr & 1;
+            tempCount += one === 1 ? 1 : 0;        
+            curr = curr >>> 1;
+        }
+    
+        counts.push(tempCount);
+    }
+      
+    return counts;
+};
