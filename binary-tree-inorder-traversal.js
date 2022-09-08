d6071b32a47f77cd1b98bc28f9bfdeae3e201db1
ravbhanage <ravbhanage@gmail.com>
2022-09-08 19:14:10 +0530

    binary tree inorder traversal problem

---
+++ binary-tree-inorder-traversal.js
@@ -0,0 +1,49 @@
+/* Binary Tree Inorder Traversal
+
+Given the root of a binary tree, return the inorder traversal of its nodes' values.
+
+Example 1:
+Input: root = [1,null,2,3]
+Output: [1,3,2]
+
+Example 2:
+Input: root = []
+Output: []
+
+Example 3:
+Input: root = [1]
+Output: [1]
+*/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+ var inorderTraversal = function(root) {
+    let nodes = [];
+    
+    if(root === null){
+        return nodes;
+    }
+    
+    function inorder(node){
+        if(node){
+            inorder(node.left);
+            nodes.push(node.val);
+            inorder(node.right);
+        }
+    }
+    
+    inorder(root);
+    
+    return nodes;
+    
+};
